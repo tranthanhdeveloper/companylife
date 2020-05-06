4607649260a2191902be63b09659476667ebfcad
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-05-06 20:13:05 +0700

    Initial commit

---
+++ src/main/java/com/programmingsharing/companylife/CompanylifeApplication.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.companylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanylifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanylifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programmingsharing/companylife/CompanylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.companylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanylifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
